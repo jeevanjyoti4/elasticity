f03711a1446187c638d4a13311d3ef8e9a4fadbb
Jeevan Chakraborty <jeevanjyoti4@gmail.com>
2020-10-10 20:52:39 +0530

    First big set of modifications in 2020
    * Divided Defns_and_PressureVessel.ipynb into
    PolarCoordinates_Definitions and Axisymmetric_PressureVessel
    
    * `import sympy as sp` is changed to `import sympy as sym`
    
    * Both PolarCoordinates_Definitions and Axisymmetric_PressureVessel
    files have YouTube lecture videos embedded in them.

--- polarUtilities.py
+++ polarUtilities.py
@@ -1,45 +1,32 @@
-import sympy as sp
+import sympy as sym
 
-r, theta = sp.symbols('r, theta')
+r, theta = sym.symbols('r, theta')
 
-f = sp.Function('f')(r,theta)
+delr_delx = sym.cos(theta)
 
+delr_dely = sym.sin(theta)
 
-def delr_delx():
-    return sp.cos(theta)
-    
-def delr_dely():
-    return sp.sin(theta)
+deltheta_delx = -sym.sin(theta)/r
 
-def deltheta_delx():
-    return -sp.sin(theta)/r
-
-def deltheta_dely():
-    return sp.cos(theta)/r
-
-
+deltheta_dely = sym.cos(theta)/r
 
 def del_delx(f):
-    return delr_delx()*sp.diff(f,r,1) + deltheta_delx()*sp.diff(f,theta,1)
+    return delr_delx*sym.diff(f,r) + deltheta_delx*sym.diff(f,theta)
 
 def del_dely(f):
-    return delr_dely()*sp.diff(f,r,1) + deltheta_dely()*sp.diff(f,theta,1)
+    return delr_dely*sym.diff(f,r) + deltheta_dely*sym.diff(f,theta)
 
 def del2_delx2(f):
     return del_delx(del_delx(f))
 
-
 def del2_dely2(f):
     return del_dely(del_dely(f))
 
-
 def polarLaplacian(f):
-    return (del2_delx2(f)+del2_dely2(f)).simplify()
-
+    return (del2_delx2(f) + del2_dely2(f)).simplify()
 
 def polarbiharmonic(f):
     return polarLaplacian(polarLaplacian(f))
-
 
 def sigma_xx(f):
     return del2_dely2(f)
@@ -49,23 +36,22 @@ def sigma_yy(f):
 
 def sigma_xy(f):
     return -del_delx(del_dely(f))
-    
+
+
 def sigma_rect(f):
-    return sp.Matrix([[sigma_xx(f), sigma_xy(f)],[sigma_xy(f), sigma_yy(f)]])
-    
+    return sym.Matrix([[sigma_xx(f), sigma_xy(f)],[sigma_xy(f), sigma_yy(f)]])
 
-Q = sp.Matrix([[sp.cos(theta), sp.sin(theta)],[-sp.sin(theta), sp.cos(theta)]])
 
+Q = sym.Matrix([[sym.cos(theta), sym.sin(theta)],[-sym.sin(theta), sym.cos(theta)]])
 
 def sigma_polar(f):
     return Q*sigma_rect(f)*(Q.T)
-
 
 def sigma_rr(f):
     return (sigma_polar(f)[0,0]).simplify().expand()
 
 def sigma_tt(f):
-    return (sigma_polar(f)[1,1]).simplify().expand()
+    return (sigma_polar(f)[1,1]).simplify()
 
 def sigma_rt(f):
     return (sigma_polar(f)[0,1]).simplify().expand()
